21327afd8273f02a0f8becbc2b21cdfa776eebca
monologg <adieujw@gmail.com>
2020-04-15 03:20:02 +0900

    [BUG FIX] Only use one bucket for multiple instance

--- build_tfdata.sh
+++ build_tfdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python3 build_pretraining_dataset.py --corpus-dir data \
-                                     --vocab-file vocab-32000-ch-6000.txt \
+                                     --vocab-file vocab.txt \
                                      --output-dir pretrain_tfrecords \
-                                     --max-seq-length 512 \
+                                     --max-seq-length 128 \
                                      --num-processes 4 \
                                      --no-lower-case
--- run_small_electra.sh
+++ run_small_electra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_pretraining.py --data-dir gs://electra-small-v2 --model-name electra-small --hparams small_config.json
+python3 run_pretraining.py --data-dir gs://electra-data-monologg --model-name electra-small --hparams small_config.json
