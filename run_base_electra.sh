abb4b56635d14ff4af36791b0dc45a5b5ffdef1c
monologg <adieujw@gmail.com>
2020-04-14 18:20:03 +0900

    typo

--- run_base_electra.sh
+++ run_base_electra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_pretraining --data-dir gs://electra-data-monologg --model-name electra-base --hparams base_config.json
+python3 run_pretraining.py --data-dir gs://electra-data-monologg --model-name electra-base --hparams base_config.json
