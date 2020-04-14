b32806bb734f2f6112067bb5ac865fc0fae2e22a
monologg <adieujw@gmail.com>
2020-04-14 21:27:04 +0900

    Update gs name

--- run_base_electra_big_batch.sh
+++ run_base_electra_big_batch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_pretraining.py --data-dir gs://electra-base-512-monologg --model-name electra-base-big-batch --hparams base_config_big_batch.json
+python3 run_pretraining.py --data-dir gs://electra-base-big-batch-monologg --model-name electra-base-big-batch --hparams base_config_big_batch.json
