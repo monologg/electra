b826d48029d48bb694b48f107f02e385810c1132
monologg <adieujw@gmail.com>
2020-04-14 17:48:59 +0900

    Update basline

--- build_tfdata.sh
+++ build_tfdata.sh
@@ -2,7 +2,7 @@
 
 python3 build_pretraining_dataset.py --corpus-dir data \
                                      --vocab-file vocab-32000-ch-6000.txt \
-                                     --output-dir tfdata \
+                                     --output-dir pretrain_tfrecords \
                                      --max-seq-length 512 \
                                      --num-processes 4 \
                                      --no-lower-case
