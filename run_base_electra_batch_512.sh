657f01f6108961ba31d069aa9752bbfcc63f1fca
monologg <adieujw@gmail.com>
2020-04-14 20:18:28 +0900

    Change bucket address for big batch

--- run_base_electra_batch_512.sh
+++ run_base_electra_batch_512.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_pretraining.py --data-dir gs://electra-data-monologg --model-name electra-base-big-batch --hparams base_config_batch_512.json
+python3 run_pretraining.py --data-dir gs://electra-base-512-monologg --model-name electra-base-big-batch --hparams base_config_batch_512.json
