4a205870b2c1759a88d550e2170ff9d5a4e4fb34
monologg <adieujw@gmail.com>
2020-04-16 02:14:25 +0900

    Erase small 128

--- run_small_electra_128.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 run_pretraining.py --data-dir gs://electra-data-128 --model-name electra-small-128 --hparams small_config_128.json
---
+++ run_small_electra_v2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 run_pretraining.py --data-dir gs://electra-data-monologg --model-name electra-small-v2 --hparams small_config_v2.json
