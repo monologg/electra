f26adcf7dae431a9262c671eb289bad4447891b8
monologg <adieujw@gmail.com>
2020-04-17 02:55:24 +0900

    Update config

--- script/run_small_electra.sh
+++ script/run_small_electra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_pretraining.py --data-dir gs://koelectra-char-data --model-name electra-small-v1 --hparams config/small_config.json
+python3 run_pretraining.py --data-dir gs://koelectra-char-data --model-name koelectra-char-small-v1 --hparams config/small_config.json
