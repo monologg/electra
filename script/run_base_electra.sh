7fc54b85f627148ddd619e28771385593d88d393
monologg <adieujw@gmail.com>
2020-05-19 21:55:44 +0900

    update

--- script/run_base_electra.sh
+++ script/run_base_electra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_pretraining.py --data-dir gs://koelectra-mecab-data --model-name koelectra-mecab-base --hparams config/base_config.json
+python3 run_pretraining.py --data-dir gs://koelectra-mecab-data --model-name koelectra-mecab-base-big-gen --hparams config/base_config.json
